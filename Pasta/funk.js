65d8aef2c67544cb87ca170fbfc33328287078e1
Joshua Bourton <joshua.bourton16@gmail.com>
2022-11-07 11:37:55 +0000

    Correct minor spelling mistake in funk.js

--- Pasta/funk.js
+++ Pasta/funk.js
@@ -1,23 +1,5 @@
 let primaries = document.querySelectorAll(".text-primary");
-let h1 = document.querySelector("h1");
-h1.innerHTML = "PASTA PASTA PASTA";
 
-<<<<<<< HEAD
-let dictionary = document.getElementById("pasta_dictionary");
-let newDictElt = document.createElement("li");
-dictionary.appendChild(newDictElt);
-newDictElt.innerHMTL = "Pasta was originally made in China";
-
-let dElts = document.querySelectorAll("#pasta_dictionary > li");
-
-for(let dElt of dElts){
-    dElt.addEventListener("click", () => dElt.classList.add("text-secondary"))
-}
-
-// for(let primary of primaries){
-//    primary.addEventListener("mouseover", () => alert("Ooh I love pasta me"))
-// }
-=======
 for(let primary of primaries){
     primary.addEventListener("mouseover", () => alert("Ooh I love pasta me"))
 }
@@ -35,4 +17,3 @@ let dElts = document.querySelectorAll("#pasta_dictionary > li")
 for(let dElt of dElts){
     dElt.addEventListener("click", () => dElt.classList.add("text-danger"))
 }
->>>>>>> 8ef79491256fdbd38d2915b82ead902bb369cdac
