a76d5688eea0e4d0e39b1be3cbd6d1d065ceba50
Joshua Bourton <joshua.bourton16@gmail.com>
2022-11-07 10:39:15 +0000

    Made spelling correction to opinion about pasta

--- Pasta/funk.js
+++ Pasta/funk.js
@@ -1,5 +1,8 @@
 let primaries = document.querySelectorAll(".text-primary")
+let h1 = document.querySelector("h1")
+h1.innerHTML = "PASTA PASTA PASTA"
 
+// let dictionary = document.getElementById()
 
 
 for(let primary of primaries){
