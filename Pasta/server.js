4c410d09fa21aecbeb392f9b2b4da13af240053a
Derg <94351943+Dergg@users.noreply.github.com>
2022-11-21 10:28:57 +0000

    Pasta page now loads on server
    Server will now return the index.html file with no missing assets.

--- Pasta/server.js
+++ Pasta/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-app.use(express.static('client'));
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'client')));
 
 const recipes =
 {
@@ -8,18 +9,19 @@ const recipes =
 "bolognese" : "Meaty tomato good with spaghetti"
 }
 
+app.get('/', function(req,res){
+    res.render('./index.html');
+    //__dirname should resolve to the project folder.
+  });
+  
+
 app.get('/recipes', function(req, resp){
     const recipe = req.query.recipe;
     let instructions = recipes[recipe];
-    resp.send(`<h1>This is a recipe for ${recipe}</h1><p>${instructions}</p>`)
-
+    resp.send(`<h1>This is a recipe for ${recipe}</h1><p>${instructions}</p>`);
 })
-app.listen(8090);
-
-const path = require('path');
-const router = express.Router();
-
-router.get('/',function(req,res){
-  res.render(path.join(__dirname+'/client/index.html'));
-  //__dirname should resolve to the project folder.
-});
+var server = app.listen(3000, function (){
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+  });
