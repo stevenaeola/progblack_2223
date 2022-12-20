11497cf161d01239fa0b59319b130c92c39f32ed
dcs0spb <s.p.bradley@durham.ac.uk>
2022-12-20 16:05:01 +0000

    Add recipe works from client side

--- Pasta/app.js
+++ Pasta/app.js
@@ -3,37 +3,31 @@ const app = express();
 
 const fs = require('fs');
 
-const fileNameForJSON = "./recipes.json"
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
+const fileNameForJSON = './recipes.json';
+app.use(express.json());
 const path = require('path');
-const { builtinModules } = require('module');
-
 app.use(express.static(path.join(__dirname, 'client')));
 
-let recipes = require(fileNameForJSON)
+const recipes = require(fileNameForJSON);
 
-
-app.get('/recipe/:recipe', function(req, resp){
+app.get('/recipe/:recipe', function (req, resp) {
     const recipe = req.params.recipe;
-    let instructions = recipes[recipe];
+    const instructions = recipes[recipe];
 
     resp.send(instructions);
+});
 
-})
-
-app.get('/recipes', function(req, resp){
-    let recipeKeys = Object.keys(recipes);
+app.get('/recipes', function (req, resp) {
+    const recipeKeys = Object.keys(recipes);
     resp.send(recipeKeys);
-})
+});
 
-app.post("/recipe/new", function(req,resp){
-    let key = req.body.key;
-    let instructions = req.body.instructions;
+app.post('/recipe/new', function (req, resp) {
+    const key = req.body.key;
+    const instructions = req.body.instructions;
     recipes[key] = instructions;
     fs.writeFileSync(fileNameForJSON, JSON.stringify(recipes));
-
     resp.send(recipes);
-})
+});
 
-module.exports = app
+module.exports = app;
--- Pasta/client/recipe_search.js
+++ Pasta/client/recipe_search.js
@@ -1,42 +1,48 @@
-let search_button = document.getElementById('recipe_search_submit')
+const endpointRoot = 'http://127.0.0.1:8090/';
 
-/*search_button.addEventListener('click',function(){
-    fetch()
-})*/
-
-async function listRecipes(){
-    let recipe_content = document.getElementById("recipe_results")
-    let recipeResponse = await fetch("http://127.0.0.1:8090/recipes");
-    let recipeKeysText = await recipeResponse.text();
-    let recipeKeys = JSON.parse(recipeKeysText);
-    let recipeListElt = document.getElementById('recipeList');
-    let list = "";
-    for(let recipeKey of recipeKeys){
+async function listRecipes () {
+    const recipeResponse = await fetch(endpointRoot + 'recipes');
+    const recipeKeysText = await recipeResponse.text();
+    const recipeKeys = JSON.parse(recipeKeysText);
+    const recipeListElt = document.getElementById('recipeList');
+    let list = '';
+    for (const recipeKey of recipeKeys) {
         list += `<li class='recipe_list_item'>${recipeKey}</li>`;
     }
     recipeListElt.innerHTML = list;
-    listItems = document.querySelectorAll(".recipe_list_item");
-    for (let listItem of listItems) {
-        listItem.addEventListener("click", (event) => loadRecipe(event.target.textContent))
+    const listItems = document.querySelectorAll('.recipe_list_item');
+    for (const listItem of listItems) {
+        listItem.addEventListener('click', (event) => loadRecipe(event.target.textContent));
     }
-    
-
 }
 
-async function loadRecipe(recipeKey){
-    let recipeResponse = await fetch(`http://127.0.0.1:8090/recipe/${recipeKey}`);
-    let recipeContent = await recipeResponse.text();
-    document.getElementById("recipe_results").innerHTML=recipeContent;
+async function loadRecipe (recipeKey) {
+    const recipeResponse = await fetch(`http://127.0.0.1:8090/recipe/${recipeKey}`);
+    const recipeContent = await recipeResponse.text();
+    document.getElementById('recipe_results').innerHTML = recipeContent;
 }
 
-async function addRecipes(){
-
-let recipeForm = document.getElementById("recipe-submit");
-recipeForm.addEventListener('submit'){
-    let data = new FormData(recipeForm);
-    alert(data);
+async function addRecipes () {
+const recipeForm = document.getElementById('recipe-submit');
+recipeForm.addEventListener('submit', async function (event) {
+    event.preventDefault();
+    // eslint-disable-next-line no-undef
+    const data = new FormData(recipeForm);
+/* conversion from FormData to JSON at https://stackoverflow.com/questions/41431322/how-to-convert-formdata-html5-object-to-json */
+    const dataJSON = JSON.stringify(Object.fromEntries(data));
+    // eslint-disable-next-line no-unused-vars
+    const response = await fetch(endpointRoot + 'recipe/new',
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        body: dataJSON
+    });
+    listRecipes();
+    recipeForm.reset();
+});
 }
-}
 
-document.addEventListener("DOMContentLoaded", listRecipes);
-document.addEventListener("DOMContentLoaded", addRecipes);
+document.addEventListener('DOMContentLoaded', listRecipes);
+document.addEventListener('DOMContentLoaded', addRecipes);
